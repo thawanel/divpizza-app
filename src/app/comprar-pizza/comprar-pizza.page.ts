3d94f3592ec0ca7f1247b1b4ccde80799b4f413b
THAWANE LOPES VIEIRA DA SILVA <587176556@alunos.etec.itaqua>
2019-09-30 22:20:51 -0300

    Adicionado preços e butons de divisão

--- src/app/comprar-pizza/comprar-pizza.page.ts
+++ src/app/comprar-pizza/comprar-pizza.page.ts
@@ -7,12 +7,40 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./comprar-pizza.page.scss'],
 })
 export class ComprarPizzaPage implements OnInit {
+  idPizza;
+  nomePizza;
+  descricaoPizza;
+  precoPizza;
+  qtdPizza = 1;
+  precoPizzaFixo;
 
   constructor(private activateRoute:ActivatedRoute) {
-    this.activateRoute.snapshot.params.id
+  
    }
 
   ngOnInit() {
   }
+  ionViewDidEnter(){
+    this.idPizza = this.activateRoute.snapshot.params.id
+    let pizzaString = localStorage.getItem(this.idPizza) //pega o item no localStorage 
+    let pizzaObjeto = JSON.parse(pizzaString) // transforma em objeto o que era string
+    this.nomePizza = pizzaObjeto.nomePizza;
+    this.descricaoPizza = pizzaObjeto.descricaoPizza;
+    this.precoPizza = pizzaObjeto.precoPizza
+    this.precoPizzaFixo = this.precoPizza
 
+  }
+  dividirPizza(btn){
+    if (btn == '+'){
+        this.qtdPizza++
+        this.precoPizza =  (JSON.parse(this.precoPizzaFixo) / this.qtdPizza).toFixed(2)
+    }else{
+      if (this.qtdPizza != 1 ) {
+      this.qtdPizza--
+      this.precoPizza =  (JSON.parse(this.precoPizzaFixo) / this.qtdPizza).toFixed(2)
+
+      }
+    }
+  }
+  
 }
